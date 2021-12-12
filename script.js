e53e7ece4eaaa9cd2e7e04c91b79e26d42ffc2ad
Thomas-Welsby <89085716+Thomas-Welsby@users.noreply.github.com>
2021-12-12 22:55:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var appear = function() {
+  document.getElementById('elementToFadeInAndOut').style.display = 'none';
+}
+window.onload = function(){
+  setTimeout(appear , 5400);
+}
+
+var appearForum = function() {
+  document.getElementById('content').style.display = 'block';
+
+}
+window.onload = function(){
+  setTimeout(appearForum , 6000);
+}
+
+function f1(){
+  alert("Now please click send on the email pop up , so we can recieve your info for a chance to enter the giveaway on the brand new Penis 360 Plus");
+}
